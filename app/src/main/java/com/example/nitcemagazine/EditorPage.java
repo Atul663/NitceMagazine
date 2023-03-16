eee4fdc32eee803f8c57477e14760baf76bede55
ksach <ksachink87@gmail.com>
2023-03-16 20:26:11 +0530

    Reviewer Page and Editor Page

--- app/src/main/java/com/example/nitcemagazine/EditorPage.java
+++ app/src/main/java/com/example/nitcemagazine/EditorPage.java
@@ -1,14 +1,137 @@
 package com.example.nitcemagazine;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EditorPage extends AppCompatActivity {
 
+    ImageView articleImage;
+    TextView articletitle;
+    TextView description;
+    RatingBar ratingBar;
+    TextView ratingSummary;
+    Button reject;
+    Button post;
+
+    String ArticleId;
+    DatabaseReference dbreference;
+
+    String imageUrl="";
+    int reviewCount=0;
+    float Rating=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor_page);
+
+        articleImage=(ImageView) findViewById(R.id.articleImage);
+        articletitle=(TextView) findViewById(R.id.Title);
+        description=(TextView) findViewById(R.id.articleText);
+        ratingBar=(RatingBar) findViewById(R.id.ratingBar);
+        ratingSummary=(TextView) findViewById(R.id.rating);
+        reject=(Button) findViewById(R.id.rejectPost);
+        post=(Button) findViewById(R.id.postArticle);
+
+        dbreference= FirebaseDatabase.getInstance().getReference();
+
+        ArticleId = getIntent().getStringExtra("article_id");
+
+        dbreference.child("Articles").child(ArticleId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //get Values from database
+                imageUrl = snapshot.child("imageUrl").getValue(String.class);
+                String title = snapshot.child("title").getValue(String.class);
+                String content = snapshot.child("content").getValue(String.class);
+
+                //set Values into Views
+                Picasso.get().load(imageUrl).into(articleImage);
+                articletitle.setText(title);
+                description.setText(content);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(EditorPage.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dbreference.child("reviews").child(ArticleId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //get Values from database
+                reviewCount = snapshot.child("count").getValue(Integer.class);
+                Rating = snapshot.child("rating").getValue(Float.class);
+
+                //set Values into Views
+                ratingBar.setRating(Rating);
+                String summ=Rating+"/5 reviewed by "+reviewCount+" reviewers";
+                ratingSummary.setText(summ);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(EditorPage.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        reject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbreference.child("Articles").child(ArticleId).removeValue();
+            }
+        });
+
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String,Object> map=new HashMap<>();
+                map.put("ArticleId",ArticleId);
+                map.put("title",articletitle.getText());
+                map.put("content",description.getText());
+                map.put("imageURL",imageUrl);
+                //map.put("Author",);  getAuthor Things done
+
+                //insert article in PostedArticleTable
+                dbreference.child("PostedArticles").push()
+                        .setValue(map)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(EditorPage.this, "Article Posted Successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(EditorPage.this, "error :"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                //delete from Articles Table
+                dbreference.child("Articles").child(ArticleId).removeValue();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/nitcemagazine/ReviewerPage.java
+++ app/src/main/java/com/example/nitcemagazine/ReviewerPage.java
@@ -1,14 +1,112 @@
 package com.example.nitcemagazine;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class ReviewerPage extends AppCompatActivity {
+
+
+    ImageView articleImage;
+    TextView articletitle;
+    TextView description;
+    RatingBar ratingBar;
+    Button submitButton;
+
+    String ArticleId;
+
+    DatabaseReference dbreference;
+    int reviewCount=0;
+    float prevRating=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reviewer_page);
+
+        articleImage=(ImageView) findViewById(R.id.articleImage);
+        articletitle=(TextView) findViewById(R.id.Title);
+        description=(TextView) findViewById(R.id.articleText);
+        ratingBar=(RatingBar) findViewById(R.id.ratingBar);
+        submitButton=(Button)findViewById(R.id.submitRating);
+
+
+        dbreference= FirebaseDatabase.getInstance().getReference();
+
+        ArticleId = getIntent().getStringExtra("article_id");
+
+        dbreference.child("Articles").child(ArticleId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //get Values from database
+                String imageUrl = snapshot.child("imageUrl").getValue(String.class);
+                String title = snapshot.child("title").getValue(String.class);
+                String content = snapshot.child("content").getValue(String.class);
+
+                //set Values into Views
+                Picasso.get().load(imageUrl).into(articleImage);
+                articletitle.setText(title);
+                description.setText(content);
+                ratingBar.setRating(0);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ReviewerPage.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get the current rating from the rating bar
+                float currentRating = ratingBar.getRating();
+
+                // get the number of reviews for the article
+                DatabaseReference articleRef = dbreference.child("reviews").child(ArticleId);
+
+                ValueEventListener ratingListener = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        // retrieve the rating value from the DataSnapshot
+                        reviewCount = snapshot.child("count").getValue(Integer.class);
+                        prevRating = snapshot.child("rating").getValue(Float.class);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        Toast.makeText(ReviewerPage.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                };
+                articleRef.addValueEventListener(ratingListener);
+
+                // calculate the new average rating
+                float newRating = ((currentRating + (reviewCount * prevRating)) / (reviewCount + 1));
+
+                // update the review count
+                dbreference.child("reviews").child(ArticleId).child("count").setValue(reviewCount + 1);
+
+                // update the review rating
+                dbreference.child("reviews").child(ArticleId).child("rating").setValue(newRating);
+
+                // show a toast message to indicate that the review has been submitted
+                Toast.makeText(ReviewerPage.this, "Review submitted", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
