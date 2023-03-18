c2b1bd6e9bfe100eb6dc61907b9568bdaefaacee
ksach <ksachink87@gmail.com>
2023-03-18 22:20:45 +0530

    ReviewerPage Changes

--- app/src/main/java/com/example/nitcemagazine/ReviewerPage.java
+++ app/src/main/java/com/example/nitcemagazine/ReviewerPage.java
@@ -33,29 +33,30 @@ public class ReviewerPage extends AppCompatActivity {
     int reviewCount=0;
     float prevRating=0;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reviewer_page);
-
         articleImage=(ImageView) findViewById(R.id.articleImage);
         articletitle=(TextView) findViewById(R.id.Title);
         description=(TextView) findViewById(R.id.articleText);
         ratingBar=(RatingBar) findViewById(R.id.ratingBar);
         submitButton=(Button)findViewById(R.id.submitRating);
 
-
         dbreference= FirebaseDatabase.getInstance().getReference();
 
-        ArticleId = getIntent().getStringExtra("article_id");
+        ArticleId = getIntent().getStringExtra("ArticleIdIntent");
 
         dbreference.child("Articles").child(ArticleId).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 //get Values from database
-                String imageUrl = snapshot.child("imageUrl").getValue(String.class);
+                String imageUrl = snapshot.child("ArticleImage").getValue(String.class);
+                prevRating=Float.parseFloat(snapshot.child("Rating").getValue(String.class));
+                String content = snapshot.child("description").getValue(String.class);
                 String title = snapshot.child("title").getValue(String.class);
-                String content = snapshot.child("content").getValue(String.class);
+                reviewCount =Integer.parseInt(snapshot.child("reviewCount").getValue(String.class));
 
                 //set Values into Views
                 Picasso.get().load(imageUrl).into(articleImage);
@@ -77,36 +78,20 @@ public class ReviewerPage extends AppCompatActivity {
                 float currentRating = ratingBar.getRating();
 
                 // get the number of reviews for the article
-                DatabaseReference articleRef = dbreference.child("reviews").child(ArticleId);
-
-                ValueEventListener ratingListener = new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot snapshot) {
-                        // retrieve the rating value from the DataSnapshot
-                        reviewCount = snapshot.child("count").getValue(Integer.class);
-                        prevRating = snapshot.child("rating").getValue(Float.class);
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError error) {
-                        Toast.makeText(ReviewerPage.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                };
-                articleRef.addValueEventListener(ratingListener);
+                DatabaseReference articleRef = dbreference.child("Articles").child(ArticleId);
 
                 // calculate the new average rating
                 float newRating = ((currentRating + (reviewCount * prevRating)) / (reviewCount + 1));
 
                 // update the review count
-                dbreference.child("reviews").child(ArticleId).child("count").setValue(reviewCount + 1);
+                dbreference.child("Articles").child(ArticleId).child("reviewCount").setValue(reviewCount + 1);
 
                 // update the review rating
-                dbreference.child("reviews").child(ArticleId).child("rating").setValue(newRating);
+                dbreference.child("Articles").child(ArticleId).child("Rating").setValue(newRating);
 
                 // show a toast message to indicate that the review has been submitted
                 Toast.makeText(ReviewerPage.this, "Review submitted", Toast.LENGTH_SHORT).show();
             }
         });
-
     }
 }
