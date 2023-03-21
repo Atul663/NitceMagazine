39976dff136b389acb14fce279b9aa0403303038
Atul <atulkumarsethi2001@gmail.com>
2023-03-21 21:20:47 +0530

    Delete reviewer

--- app/src/main/java/com/example/nitcemagazine/MainActivityPages/MainActivity2.java
+++ app/src/main/java/com/example/nitcemagazine/MainActivityPages/MainActivity2.java
@@ -2,6 +2,7 @@ package com.example.nitcemagazine.MainActivityPages;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -11,14 +12,19 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.nitcemagazine.AddReviewerAndEditor.AddEditor;
 import com.example.nitcemagazine.AddReviewerAndEditor.AddReviewer;
+import com.example.nitcemagazine.AddReviewerAndEditor.DeleteReviewer;
 import com.example.nitcemagazine.DeleteArticle;
 import com.example.nitcemagazine.LoginAndSignUp.LoginActivity;
 import com.example.nitcemagazine.PostArticle.AddPostFragement;
@@ -26,8 +32,14 @@ import com.example.nitcemagazine.R;
 import com.example.nitcemagazine.LoginAndSignUp.SignUpPage;
 import com.example.nitcemagazine.PostUnpostedArticle.PostUnpostedArticles;
 import com.example.nitcemagazine.ReviewUnpostedArticle.ReviewUnpostedArticles;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -41,9 +53,13 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class MainActivity2 extends AppCompatActivity {
     DrawerLayout drawerLayout;
     Toolbar toolbar;
+    AlertDialog dialog;
     NavigationView navigationView;
     ImageView navigationDrawerIcon;
     ActionBarDrawerToggle toggle;
+
+    EditText username,password;
+    Button signin;
 
     TextView emailId,role;
     CircleImageView profileProfilePicture;
@@ -112,10 +128,44 @@ public class MainActivity2 extends AppCompatActivity {
                     Intent intent = new Intent(MainActivity2.this, SignUpPage.class);
                     startActivity(intent);
                 } else if (id == R.id.addPostNavDrawer) {
-                    FragmentManager fm = getSupportFragmentManager();
-                    FragmentTransaction ft = fm.beginTransaction();
-                    ft.replace(R.id.content, new AddPostFragement()).addToBackStack("add post");
-                    ft.commit();
+                    if(user!= null)
+                    {
+                        FragmentManager fm = getSupportFragmentManager();
+                        FragmentTransaction ft = fm.beginTransaction();
+                        ft.replace(R.id.content, new AddPostFragement()).addToBackStack("add post");
+                        ft.commit();
+                    }
+                    else {
+                        AlertDialog.Builder dialogLogin = new AlertDialog.Builder(MainActivity2.this);
+                        View loginView = getLayoutInflater().inflate(R.layout.dialog_login,null);
+
+                        username = loginView.findViewById(R.id.editTextDialogUsername);
+                        password = loginView.findViewById(R.id.editTextDialogPassword);
+                        signin = loginView.findViewById(R.id.buttonDialogLogin);
+                        dialogLogin.setView(loginView);
+                        dialog = dialogLogin.create();
+                        dialog.show();
+                        signin.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                String userEmailId = username.getText().toString();
+                                String userPassword = password.getText().toString();
+
+                                if (userEmailId.isEmpty() && userPassword.isEmpty()) {
+                                    Toast.makeText(MainActivity2.this, "Please enter the Email id and Password", Toast.LENGTH_SHORT).show();
+                                } else if (userEmailId.isEmpty()) {
+                                    Toast.makeText(MainActivity2.this, "Please enter a Email id", Toast.LENGTH_SHORT).show();
+
+                                } else if (userPassword.isEmpty()) {
+                                    Toast.makeText(MainActivity2.this, "Please enter a password", Toast.LENGTH_SHORT).show();
+                                } else if (Patterns.EMAIL_ADDRESS.matcher(userEmailId).matches()) {
+                                    signInWithFirebase(userEmailId, userPassword);
+                                } else {
+                                    Toast.makeText(MainActivity2.this, "Please enter a valid email id", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
                 } else if (id == R.id.addReviewerNavDrawer) {
                     Intent intent = new Intent(MainActivity2.this, AddReviewer.class);
                     startActivity(intent);
@@ -131,10 +181,16 @@ public class MainActivity2 extends AppCompatActivity {
                     startActivity(intent);
                     finishAffinity();
                 } else if (id == R.id.postArticleNavDrawer) {
-                    Intent intent = new Intent(MainActivity2.this, DeleteArticle.class);
+                    Intent intent = new Intent(MainActivity2.this, PostUnpostedArticles.class);
                     startActivity(intent);
                 } else if (id == R.id.reviewArticleNavDrawer) {
                     Intent intent = new Intent(MainActivity2.this, ReviewUnpostedArticles.class);
+                    startActivity(intent);
+                } else if (id == R.id.deleteArticleNavDrawer) {
+                    Intent intent = new Intent(MainActivity2.this, DeleteArticle.class);
+                    startActivity(intent);
+                } else if (id == R.id.removeReviewerNavDrawer) {
+                    Intent intent = new Intent(MainActivity2.this, DeleteReviewer.class);
                     startActivity(intent);
                 }
 
@@ -252,6 +308,68 @@ public class MainActivity2 extends AppCompatActivity {
             signUp.setVisible(true);
             logout.setVisible(false);
         }
+    }
+
+    private void signInWithFirebase(String userEmailId, String userPassword) {
+        auth.signInWithEmailAndPassword(userEmailId,userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    FirebaseUser user = auth.getCurrentUser();
+                    reference.child("UserType").child(user.getUid()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            String role = snapshot.getValue().toString();
+                            if(role.equalsIgnoreCase("Admin"))
+                            {
+                                Toast.makeText(MainActivity2.this, "Sign in successful", Toast.LENGTH_SHORT).show();
+                                FragmentManager fm = getSupportFragmentManager();
+                                FragmentTransaction ft = fm.beginTransaction();
+                                ft.replace(R.id.content, new AddPostFragement()).addToBackStack("add post");
+                                ft.commit();
+                            }
+                            else if(auth.getCurrentUser().isEmailVerified())
+                            {
+                                Toast.makeText(MainActivity2.this, "Sign in successful", Toast.LENGTH_SHORT).show();
+                                FragmentManager fm = getSupportFragmentManager();
+                                FragmentTransaction ft = fm.beginTransaction();
+                                ft.replace(R.id.content, new AddPostFragement()).addToBackStack("add post");
+                                ft.commit();
+                            }
+                            else {
+                                auth.getCurrentUser().sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+
+                                        Toast.makeText(MainActivity2.this, "Please verify your email.", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                });
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+                else
+                {
+                    if(task.getException() instanceof FirebaseAuthInvalidUserException)
+                    {
+                        Toast.makeText(MainActivity2.this, "Invalid User", Toast.LENGTH_SHORT).show();
+                    }
+                    else if(task.getException() instanceof FirebaseAuthInvalidCredentialsException)
+                    {
+                        Toast.makeText(MainActivity2.this, "Password is wrong", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+            }
+        });
     }
     @Override
     public void onBackPressed() {
